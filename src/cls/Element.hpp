f770f9ecdbac6af52223fc384c336a71e1520770
Jan Zimola <Zimolajan789@gmail.com>
2022-04-25 20:20:45 +0200

    adding classe draft

---
+++ src/cls/Element.hpp
@@ -0,0 +1,21 @@
+/**
+ * class representing element in string format - to be inherited
+ * @author dr.Koci, rewritten to c++ by xzimol04
+ * */
+#pragma once
+
+#include <string>
+class Element
+{
+public:
+    std::string name;
+
+    Element(std::string name);
+    /**
+     * @brief Destroy the Element object
+     *
+     */
+    ~Element();
+
+    bool operator==(const Element &other) const;
+};
